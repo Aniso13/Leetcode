e1a187ce99f425128145a32bb2413ec5067d48d9
Mohamed Ebrahim <74831799+Aniso13@users.noreply.github.com>
2024-12-04 20:58:55 +0200

    Time: 6 ms (71.31%), Space: 45.7 MB (36.84%) - LeetHub

---
+++ 2825-make-string-a-subsequence-using-cyclic-increments/2825-make-string-a-subsequence-using-cyclic-increments.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean canMakeSubsequence(String str1, String str2) {
+        if (str2.length() > str1.length()) {
+            return false;
+        }
+
+        int j = 0;   
+
+        for (int i = 0; i < str1.length() && j < str2.length(); i++) {
+            char currentChar = str1.charAt(i);
+            char nextChar = currentChar == 'z' ? 'a' : (char) (currentChar + 1);
+
+            if (currentChar == str2.charAt(j) || nextChar == str2.charAt(j)) {
+                j++;
+            }
+        }
+
+        return j == str2.length();
+    }
+}
