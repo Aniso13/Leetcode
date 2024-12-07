e3ed2774002e07399d6bfa9e97e474a5b739d566
Mohamed Ebrahim <74831799+Aniso13@users.noreply.github.com>
2024-12-07 19:30:21 +0200

    Time: 41 ms (96.27%), Space: 112.9 MB (68.81%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.java
@@ -0,0 +1,83 @@
+import java.util.*;
+
+class LRUCache {
+    private class Node {
+        int key, value;
+        Node prev, next;
+
+        Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private final int capacity;
+    private final Map<Integer, Node> cache;
+    private final Node head, tail;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        this.cache = new HashMap<>();
+        this.head = new Node(0, 0);
+        this.tail = new Node(0, 0); 
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    public int get(int key) {
+        if (cache.containsKey(key)) {
+            Node node = cache.get(key);
+            moveToHead(node); 
+            return node.value;
+        }
+        return -1;
+    }
+
+    public void put(int key, int value) {
+        if (cache.containsKey(key)) {
+            Node node = cache.get(key);
+            node.value = value;
+            moveToHead(node); 
+        } else {
+            Node newNode = new Node(key, value);
+            cache.put(key, newNode);
+            addNode(newNode);
+
+            if (cache.size() > capacity) {
+                Node tailNode = popTail();
+                cache.remove(tailNode.key); 
+            }
+        }
+    }
+
+    private void addNode(Node node) {
+        
+        node.prev = head;
+        node.next = head.next;
+
+        head.next.prev = node;
+        head.next = node;
+    }
+
+    private void removeNode(Node node) {
+        
+        Node prev = node.prev;
+        Node next = node.next;
+
+        prev.next = next;
+        next.prev = prev;
+    }
+
+    private void moveToHead(Node node) {
+        
+        removeNode(node);
+        addNode(node);
+    }
+
+    private Node popTail() {
+        
+        Node node = tail.prev;
+        removeNode(node);
+        return node;
+    }
+}
