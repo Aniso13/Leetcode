e065eb4485f1752979deab685625b0e6511f5e77
Mohamed Ebrahim <74831799+Aniso13@users.noreply.github.com>
2024-12-13 19:17:52 +0200

    Time: 8 ms (67.54%), Space: 49.7 MB (19.83%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
@@ -1,19 +1,18 @@
 class NumArray {
-    public int nums[];
-    
+    private int[] prefixSum;
 
     public NumArray(int[] nums) {
-        this.nums=nums; // as it is a constructor
-        
-    }
-    
-    public int sumRange(int left, int right) {
-        int ans=0;
-        for(int i=left;i<=right;i++){
-            ans+=nums[i];
+        prefixSum = new int[nums.length];
+        prefixSum[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            prefixSum[i] = prefixSum[i - 1] + nums[i];
         }
-        return ans;
+    }
 
-        
+    public int sumRange(int left, int right) {
+        if (left == 0) {
+            return prefixSum[right];
+        }
+        return prefixSum[right] - prefixSum[left - 1];
     }
 }
