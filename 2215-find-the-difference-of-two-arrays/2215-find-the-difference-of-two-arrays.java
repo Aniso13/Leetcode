14c9d97706007d56c20f8406680036775585575f
Mohamed Ebrahim <74831799+Aniso13@users.noreply.github.com>
2024-12-17 21:31:08 +0200

    Time: 9 ms (96.91%), Space: 45.7 MB (20.95%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.java
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.java
@@ -1,26 +1,28 @@
+import java.util.*;
+
 class Solution {
-    // Returns the elements in the first arg nums1 that don't exist in the second arg nums2.
-    List<Integer> getElementsOnlyInFirstList(int[] nums1, int[] nums2) {
-        Set<Integer> onlyInNums1 = new HashSet<> (); 
+    public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
+        // Initialize sets for both arrays
+        HashSet<Integer> h1 = new HashSet<>();
+        HashSet<Integer> h2 = new HashSet<>();
         
-        // Store nums2 elements in an unordered set. 
-        Set<Integer> existsInNums2 = new HashSet<> (); 
-        for (int num : nums2) {
-            existsInNums2.add(num);
-        }
-        
-        // Iterate over each element in the list nums1.
-        for (int num : nums1) {
-            if (!existsInNums2.contains(num)) {
-                onlyInNums1.add(num);
+        // Populate the sets
+        for (int n : nums1) h1.add(n);
+        for (int n : nums2) h2.add(n);
+
+        // Remove common elements from both sets
+        for (int n : nums2) {
+            if (h1.contains(n)) {
+                h1.remove(n);
+                h2.remove(n);
             }
         }
+
+        // Build the result
+        List<List<Integer>> result = new ArrayList<>();
+        result.add(new ArrayList<>(h1)); // Unique to nums1
+        result.add(new ArrayList<>(h2)); // Unique to nums2
         
-        // Convert to vector.
-        return new ArrayList<>(onlyInNums1);
-    }
-    
-    public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
-        return Arrays.asList(getElementsOnlyInFirstList(nums1, nums2), getElementsOnlyInFirstList(nums2, nums1));
+        return result;
     }
 }
