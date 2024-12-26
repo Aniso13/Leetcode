7ef4127bce1aef0fbf5e0abb9896aad52e6c00c7
Mohamed Ebrahim <74831799+Aniso13@users.noreply.github.com>
2024-12-26 18:16:23 +0200

    Time: 10 ms (39.79%), Space: 42.9 MB (16.81%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
@@ -1,15 +1,28 @@
+import java.util.HashMap;
+
 class Solution {
     public int maxNumberOfBalloons(String text) {
-        int [] arr= new int [256];
-        for(int i=0;i<text.length();i++)
-            arr[text.charAt(i)]++;
-        arr['l']/=2;
-        arr['o']/=2;
-        String texts="balloon";
-        int small=arr['b'];
-        for(int i=0;i<texts.length();i++){
-            small=Math.min(small,arr[texts.charAt(i)]);
+        HashMap<Character, Integer> charCount = new HashMap<>();
+        
+        for (char c : text.toCharArray()) {
+            charCount.put(c, charCount.getOrDefault(c, 0) + 1);
         }
-        return small;
+
+        HashMap<Character, Integer> balloonMap = new HashMap<>();
+        balloonMap.put('b', 1);
+        balloonMap.put('a', 1);
+        balloonMap.put('l', 2);
+        balloonMap.put('o', 2);
+        balloonMap.put('n', 1);
+
+        int minCount = Integer.MAX_VALUE;
+        for (char c : balloonMap.keySet()) {
+            int countInText = charCount.getOrDefault(c, 0);
+            int requiredCount = balloonMap.get(c);
+
+            minCount = Math.min(minCount, countInText / requiredCount);
+        }
+
+        return minCount;
     }
 }
