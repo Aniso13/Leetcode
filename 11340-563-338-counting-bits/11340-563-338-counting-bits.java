405265f4fea306663ad8ba7efa69436113513360
Mohamed Ebrahim <74831799+Aniso13@users.noreply.github.com>
2025-01-16 20:51:20 +0200

    Time: 2 ms, Memory: 46.4 MB - LeetHub

--- 11340-563-338-counting-bits/11340-563-338-counting-bits.java
+++ 11340-563-338-counting-bits/11340-563-338-counting-bits.java
@@ -1,20 +1,11 @@
 class Solution {
-    public int[] countBits(int n) {
-        int [] res = new int [n+1];
-
-        for(int i=0 ; i<n+1;i++)
-        {
-            res[i] = hammingWeight(i);
-        }
-
-        return res;
+   public int[] countBits(int n) {
+    int[] res = new int[n + 1];
+    res[0] = 0;
+    
+    for (int i = 1; i <= n; i++) {
+        res[i] = res[i >> 1] + (i & 1);
     }
-    public int hammingWeight(int n) {
-        int count = 0;
-        while (n != 0) {
-            n = n & (n - 1);
-            count++;
-        }
-        return count;
-    }
+    return res;
+}
 }
