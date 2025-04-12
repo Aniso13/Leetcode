0dae9beb799cfaee5600909aadf8fd765a924f5b
Mohamed Ebrahim Anis <74831799+Aniso13@users.noreply.github.com>
2025-04-12 22:49:24 +0200

    Time: 135 ms (82.19%), Space: 124.8 MB (5.5%) - LeetHub

---
+++ 1023-time-based-key-value-store/1023-time-based-key-value-store.java
@@ -0,0 +1,44 @@
+class TimeMap {
+    Node head;
+
+    public TimeMap() {
+        head = new Node();
+    }
+
+    public void set(String key, String value, int timestamp) {
+        head = new Node(key, value, timestamp, head);
+    }
+
+    public String get(String key, int timestamp) {
+        return dfs(key, timestamp, head);
+    }
+
+    private String dfs(String key, int timestamp, Node node) {
+        if(node == null) return new String();
+        if(key.equals(node.key) && timestamp >= node.timestamp) return node.value;
+
+        return dfs(key, timestamp, node.prev);
+    }
+
+    class Node {
+        String key, value;
+        int timestamp;
+        Node prev;
+
+        Node(){}
+        Node(String _key, String _value, int _timestamp, Node _prev) {
+            key = _key;
+            value = _value;
+            timestamp = _timestamp;
+            prev = _prev;
+        }
+    }
+}
+
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap obj = new TimeMap();
+ * obj.set(key,value,timestamp);
+ * String param_2 = obj.get(key,timestamp);
+ */
