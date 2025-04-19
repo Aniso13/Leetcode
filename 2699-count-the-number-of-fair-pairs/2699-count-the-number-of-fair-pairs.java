eb5802acddc0a18d3a016e3c686089921d553840
Mohamed Ebrahim Anis <74831799+Aniso13@users.noreply.github.com>
2025-04-19 15:29:06 +0200

    Time: 23 ms (100%), Space: 57.3 MB (56.8%) - LeetHub

--- 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.java
+++ 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.java
@@ -3,42 +3,16 @@ import java.util.Arrays;
 class Solution {
     public long countFairPairs(int[] nums, int lower, int upper) {
         Arrays.sort(nums);
-        long count = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-            int left = lowerBound(nums, i + 1, nums.length - 1, lower - nums[i]);
-            int right = upperBound(nums, i + 1, nums.length - 1, upper - nums[i]);
-            count += (right - left + 1);
-        }
-
-        return count;
+        return count(nums, upper) - count(nums, lower - 1);
     }
 
-    private int lowerBound(int[] nums, int start, int end, int target) {
-        int low = start, high = end, ans = end + 1;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (nums[mid] >= target) {
-                ans = mid;
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
+    private long count(int[] nums, int target) {
+        long res = 0;
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            if (nums[left] + nums[right] > target) right--;
+            else res += right - left++;
         }
-        return ans;
-    }
-
-    private int upperBound(int[] nums, int start, int end, int target) {
-        int low = start, high = end, ans = start - 1;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (nums[mid] <= target) {
-                ans = mid;
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
-        }
-        return ans;
+        return res;
     }
 }
