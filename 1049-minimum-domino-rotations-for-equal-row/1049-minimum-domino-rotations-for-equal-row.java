04baf86477baf58dd4ac737561644497a8585e21
Mohamed Ebrahim Anis <74831799+Aniso13@users.noreply.github.com>
2025-05-03 15:00:43 +0300

    Time: 4 ms (84.8%), Space: 50.3 MB (64.53%) - LeetHub

--- 1049-minimum-domino-rotations-for-equal-row/1049-minimum-domino-rotations-for-equal-row.java
+++ 1049-minimum-domino-rotations-for-equal-row/1049-minimum-domino-rotations-for-equal-row.java
@@ -1,34 +1,21 @@
 class Solution {
     public int minDominoRotations(int[] tops, int[] bottoms) {
-        int [] freq = new int[7];
+        int result = check(tops[0], tops, bottoms);
+        if (result != -1) return result;
+        return check(bottoms[0], tops, bottoms);
+    }
 
-        for(int i=0;i<tops.length; i++)
-        {
-            for(int j=1;j<7;j++)
-            {
-                if(tops[i]==j || bottoms[i]==j) freq[j]++;
-            }
-        }
-        int mincounter= Integer.MAX_VALUE;
-        for(int i=1;i<7;i++)
-        {
-            if(freq[i]==tops.length)
-            {
-                int x = i;
-                int countertop = 0;
-                int counterbottom=0;
-                for(int j=0 ; j<tops.length;j++)
-                {
-                    if(tops[j]!=x) countertop++;
-                    if(bottoms[j]!=x) counterbottom++;
-                }
-                mincounter=Math.min(mincounter , countertop);
-                mincounter=Math.min(mincounter , counterbottom);
-            }
+    private int check(int target, int[] tops, int[] bottoms) {
+        int topSwaps = 0;
+        int bottomSwaps = 0;
+
+        for (int i = 0; i < tops.length; i++) {
+            if (tops[i] != target && bottoms[i] != target) return -1;
+
+            if (tops[i] != target) topSwaps++;
+            if (bottoms[i] != target) bottomSwaps++;
         }
 
-        if(mincounter == Integer.MAX_VALUE)
-            return -1;
-        return mincounter;
+        return Math.min(topSwaps, bottomSwaps);
     }
 }
