d6ac0a84aa69264c0c0fd4a0d57d80009c003596
Mohamed Ebrahim Anis <74831799+Aniso13@users.noreply.github.com>
2025-06-03 16:10:12 +0300

    Time: 5 ms (68.66%), Space: 56.5 MB (80.1%) - LeetHub

---
+++ 1424-maximum-candies-you-can-get-from-boxes/1424-maximum-candies-you-can-get-from-boxes.java
@@ -0,0 +1,49 @@
+class Solution {
+    public int maxCandies(int[] status, int[] candies, int[][] keys, int[][] containedBoxes, int[] initialBoxes) {
+
+        int n = status.length;
+
+        boolean [] hasBox = new boolean[n];
+        boolean [] hasKey = new boolean[n];
+        boolean [] visited = new boolean[n];
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        for(int box : initialBoxes) {
+            hasBox[box] = true;
+
+            if(status[box]==1) {
+                queue.offer(box);
+                visited[box] = true;
+            }
+        }
+        int totalCandies = 0;
+
+        while(!queue.isEmpty()) {
+            int curr = queue.poll();
+            totalCandies += candies[curr];
+
+            for(int key : keys[curr]){
+                hasKey[key] = true;
+
+                if(hasBox[key] && !visited[key]){
+                    queue.offer(key);
+                    visited[key] = true;
+                }
+            }
+
+            for (int box : containedBoxes[curr]) {
+                hasBox[box] = true;
+                if (status[box] == 1 || hasKey[box]) {
+                    if (!visited[box]) {
+                        queue.offer(box);
+                        visited[box] = true;
+                    }
+                }
+            }
+        }
+
+        return totalCandies;
+        
+    }
+}
